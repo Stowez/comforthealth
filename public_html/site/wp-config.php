c4fcac8580b709503ea848975786e2d9d2a85f0f
Thomas Stowe <tomstowe@stoweb.localdomain>
2017-01-21 16:47:30 +0000

    loads of new uplaods

--- public_html/site/wp-config.php
+++ public_html/site/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'comforthealth');
+define('DB_NAME', 'comforth_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'comforth_user');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'adkgk3gk3avkaKAF');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
