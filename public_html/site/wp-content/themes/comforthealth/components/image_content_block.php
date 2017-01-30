bf4b6e71071e2ad6826cfe11315eb6e548370d5f
Tom Stowe <tom@razorcreations.com>
2017-01-30 15:25:33 +0000

    block image vertical align

--- public_html/site/wp-content/themes/comforthealth/components/image_content_block.php
+++ public_html/site/wp-content/themes/comforthealth/components/image_content_block.php
@@ -3,20 +3,26 @@
 	if ( get_sub_field('image_position') == 'left' ) :?>
 		<div class="half-block with-image"  style="background-image:url('<? the_sub_field('background_image'); ?>');"></div>
 		<div class="half-block with-content">
-		<?php
-			the_sub_field('content');
-		?>
-		<? if (get_sub_field('link') ){ ?>
-			<a href="<? the_sub_field('link'); ?>" class="btn outline">Read more</a>
-		<?}?>
+			<div>
+				<?php
+					the_sub_field('content');
+				?>
+				<? if (get_sub_field('link') ){ ?>
+					<a href="<? the_sub_field('link'); ?>" class="btn outline">Read more</a>
+				<?}?>
+			</div>
+			<i></i>
 		</div>
 	<?else:?>
 		<div class="half-block with-content">
-		<?php the_sub_field('content'); ?>
-		<?php $link = the_sub_field('link');?>
-		<? if (get_sub_field('link') ){ ?>
-			<a href="<? the_sub_field('link'); ?>" class="btn outline">Read more</a>
-		<?}?>
+			<div>
+				<?php the_sub_field('content'); ?>
+				<?php $link = the_sub_field('link');?>
+				<? if (get_sub_field('link') ){ ?>
+					<a href="<? the_sub_field('link'); ?>" class="btn outline">Read more</a>
+				<?}?>
+			</div>
+			<i></i>
 		</div>
 		<div class="half-block with-image"  style="background-image:url('<? the_sub_field('background_image'); ?>');"></div>
 	<?endif;
