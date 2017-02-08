d4175ccd8423b5c6a5685ce3b0a44181bb5d318e
Tom Stowe <tom@razorcreations.com>
2017-02-08 21:39:25 +0000

    404 making booking comp

--- public_html/wp-content/themes/comforthealth/components/featured_header.php
+++ public_html/wp-content/themes/comforthealth/components/featured_header.php
@@ -1,8 +1,3 @@
 <section class="featured-header content--center" style="background-image: url('<? the_sub_field('background_image'); ?>')";>
-	
-	<? if (get_sub_field('link_to_booking') ) { ?>
-		<h2 class="booking"><a href="/booking"><? the_sub_field('title'); ?></a></h2>
-	<?} else {?>
 		<h2><? the_sub_field('title'); ?></h2>
-	<?}?>
 </section>
