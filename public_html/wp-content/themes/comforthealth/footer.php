859836c9d81c1bf376464417255b6aaa786575c7
Tom Stowe <tom@razorcreations.com>
2017-03-12 13:30:39 +0000

    added the plugin and new footer

--- public_html/wp-content/themes/comforthealth/footer.php
+++ public_html/wp-content/themes/comforthealth/footer.php
@@ -1,3 +1,25 @@
+<div class="footerSocial">
+	<div class="container">
+		<div class="row with-gutter">
+			<div class="col col-4">
+				<?php dynamic_sidebar( 'footer_1' ); ?>
+			</div>
+			<div class="col col-4">
+				<h4>From Twitter</h4>
+				<a class="twitter-timeline" data-width="100%" data-height="350" data-dnt="true" href="https://twitter.com/ComfortHealth_">Tweets by ComfortHealth_</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</div>
+			<div class="col col-4">
+				<h4>Newsletter Signup</h4>
+				<div class="newsletter">
+				<p>For the latest special offers and news about Comfort Health enter your email below.</p>
+				<? gravity_form(2, false, false, false, false, true); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
 <div class="footerNav">
 	<div class="container">
 		<div class="row">
@@ -25,5 +47,20 @@
 		<p>Comfort Health All Rights Reserved</p>
 	</div>
 </footer>
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
 <? wp_footer();?>
