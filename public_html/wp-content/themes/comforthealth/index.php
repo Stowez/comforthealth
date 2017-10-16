ab89954b971052970c7d2d97e1c65d1be0598f8f
Thomas Stowe <tomstowe@stoweb.localdomain>
2017-10-16 11:50:52 +0000

    page nav

--- public_html/wp-content/themes/comforthealth/index.php
+++ public_html/wp-content/themes/comforthealth/index.php
@@ -43,11 +43,11 @@
 							<h2 class="blog__title"><a href="<? the_permalink(); ?>"><? the_title(); ?></a></h2>	
 						</header>
 						<? the_excerpt(); ?>	
-						<footer>
-							<? $grav_url = "https://www.gravatar.com/avatar/" . md5( strtolower( trim( get_the_author_meta('email') ) ) ) . "?s=50"?>
-							<span class="avatar"><img src="<?= $grav_url; ?>"> By <?=get_the_author_meta('display_name');?></span>
-
-							<a href="<? the_permalink();?> " class="btn right">Read more</a>
+						<footer style="position:relative; padding: 2rem 0;">
+							<?/* $grav_url = "https://www.gravatar.com/avatar/" . md5( strtolower( trim( get_the_author_meta('email') ) ) ) . "?s=50"?>
+							<span class="avatar"><img src="<?= $grav_url; ?>">*/?>
+							 By <?=get_the_author_meta('display_name');?></span>
+							<a href="<? the_permalink();?> " class="btn right" style="margin-top:20px">Read more</a>
 						</footer>
 					</div>
 				</article>
@@ -59,12 +59,15 @@
 		endwhile;?>
 		</div></section>
 	</section>
+		<div class="navigation-pagination">
 		<?// Previous/next page navigation.
-		/*the_posts_pagination( array(
+		the_posts_pagination( array(
 			'prev_text'          => __( 'Previous page', 'twentysixteen' ),
 			'next_text'          => __( 'Next page', 'twentysixteen' ),
 			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-		) );*/
+		) );?>
+		</div>
+<?
 endif;
 
 get_footer();
