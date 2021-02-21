fa622382c76fd9a0559c046cbf7ebe436eda0356
Tom Stowe <tom@razorcreations.com>
2021-02-21 22:50:48 +0000

    video block

--- public_html/wp-content/themes/comforthealth/components/video_block.php
+++ public_html/wp-content/themes/comforthealth/components/video_block.php
@@ -1,6 +1,6 @@
 <div class="video-block" >
 	<div class="">
-		<h2 class="video-block__title">Comfort Health Clinic - Clifton</h2>
+		<h2 class="video-block__title"><h2 class="video-block__title">Comfort Health Clinic - Clifton</h2></h2>
 		<div class="video-container">
 			<iframe class="video-component" width="1920" height="1080" src="https://www.youtube.com/embed/t25gS8Mjlak?modestbranding=1&autoplay=1&fs=0&loop=1&rel=0&mute=1&showinfo=0&disablekb=1&transparent=0" frameborder="0"></iframe>
 		</div>
